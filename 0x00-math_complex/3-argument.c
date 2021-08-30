68c30bb6ad686cba34d8605f8d0f865384b0dd3f
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2021-08-30 15:38:50 +0100

    Add files via upload

---
+++ 0x00-math_complex/3-argument.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "holberton.h"
+double argument(complex c){
+    double a;
+    if (c.re == 0 && c.im == 0)
+    {
+        a = 0;
+    }
+    else if (c.re != 0)
+    {
+        if ((c.re < 0) && (c.im < 0))
+        {
+            a = atan(c.im / c.re) - M_PI ;
+        }
+        else if ((c.re < 0) && (c.im > 0))
+        {
+            a = atan(c.im / c.re) + M_PI;
+        }
+        else
+        {
+            a = atan(c.im / c.re);
+        }
+    }
+    else
+    {
+        if (c.im < 0)
+        {
+            a = -M_PI / 2;
+        }
+        else
+        {
+            a = M_PI / 2;
+        }
+    }
+    return a;
+}
--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -10,4 +10,5 @@ typedef struct
 void display_complex_number(complex c);
 complex conjugate(complex c);
 double modulus(complex c);
+double argument(complex c);
 #endif 
