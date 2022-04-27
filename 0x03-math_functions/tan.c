a02f80a22d9ca0ab13c6e86621319c04fed7662a
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2022-04-27 15:12:53 +0100

    Update tan.c

--- 0x03-math_functions/tan.c
+++ 0x03-math_functions/tan.c
@@ -7,7 +7,7 @@ int main()
   double theta;
   FILE *fp1 = NULL;
   fp1 = fopen("tan.txt", "w");
-  for (theta = 0; theta <= 6 * M_PI; theta = theta + 0.01) 
+  for (theta = -M_PI; theta <=M_PI; theta = theta + 0.01) 
   fprintf(fp1, "%lf\t%lf\n", theta, tan(theta));
   system("gnuplot -p -e \" set terminal png ; set output 'tan.png'; plot 'tan.txt' w l \"");
 }
