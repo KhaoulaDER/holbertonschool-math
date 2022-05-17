9e1e54269c40780db15791f1eea4060c0332dee1
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2022-05-17 10:38:30 +0100

    Update 1-power.py

--- 0x06-Math_Eigenvalues_and_Eigenvectors/1-power.py
+++ 0x06-Math_Eigenvalues_and_Eigenvectors/1-power.py
@@ -7,13 +7,9 @@ def power(A, tol):
     Ax = np.dot(A, x)
     nAx = np.linalg.norm(Ax)
     l = nAx / np.linalg.norm(x)
-    i=1
     while(np.linalg.norm(Ax - l * x) > tol):
         x = Ax / nAx
         Ax = np.dot(A, x)
         nAx = np.linalg.norm(Ax)
-        l = nAx / np.linalg.norm(x)
-        print('Iteration no. ', i) 
-        print(x)
-        i= i+1        
+        l = nAx / np.linalg.norm(x)              
     return l, x
